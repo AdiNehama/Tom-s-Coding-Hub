913dd9a3ed5df792d0ffa69495a8f9766d27d176
AdiNehama <133670207+AdiNehama@users.noreply.github.com>
2024-12-18 20:54:00 +0200

    Update index.js

--- TomsCodeApp/Backend/dist/index.js
+++ TomsCodeApp/Backend/dist/index.js
@@ -84,6 +84,6 @@ io.on("connection", (socket) => {
     });
 });
 // הרצת השרת
-httpServer.listen(PORT, '0.0.0.0', () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
