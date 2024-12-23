56f62b814bd5636d1a541d1b61e20597130badfe
AdiNehama <133670207+AdiNehama@users.noreply.github.com>
2024-12-23 16:37:06 +0200

    Update index.ts

--- TomsCodeApp/Backend/index.ts
+++ TomsCodeApp/Backend/index.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use("/frontend", express.static(path.join(__dirname, 'Frontend/dist')));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend','dist' , 'index.html'));
 });
 
 
